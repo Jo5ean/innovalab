32d5eadb5aa4cf92d4d99ced55daeef30a5d2117
luiscruzz.developer <luiscruzz.developer@gmail.com>
2025-04-11 20:14:07 -0300

    Eliminar la configuración del adaptador de Node y ajustes en la configuración de Vite en astro.config.mjs. Se simplificó la configuración al eliminar la salida del servidor y la gestión de HMR.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
 
-console.log({ prod: import.meta.env.PROD });
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
   integrations: [tailwind()],
-  vite: {
-    server: {
-      allowedHosts: true,
-      hmr: import.meta.env.PROD ? false : true, // Deshabilita HMR solo en producción
-    }
-  }
 });
