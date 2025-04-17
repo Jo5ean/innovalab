18b850c059d6fd1b8a8cb7b5e9566dfa76cbd41f
luiscruzz.developer <luiscruzz.developer@gmail.com>
2025-04-17 00:17:02 -0300

    Actualizar la configuración de Astro para incluir la URL base y ajustar las referencias de imagen en las metatags. Modificar el componente Layout para eliminar la propiedad de imagen y simplificar su uso en las páginas de contacto e índice.

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,10 +3,11 @@ const config = {
     site_name: "influencerfunds",
     description: "Influencer Funds enables fast, secure payments to influencers and agencies in 80+ countries and 20+ currencies, simplifying financial management.",
     theme_color: "#00FFFF",
+    baseURL: 'https://influencerfunds.com/',
     keywords: "influencer payments, global payments, marketing agencies, payment management, multi-currency payments, tax compliance, creator finances",
     author: "Influencer Funds",
     email: "contact@influencerfunds.com",
-    image: "/logos/logo.webp",
+    image: "/images/cover.webp",
     IS_PRODUCTION: Boolean(Number(import.meta.env.IS_PRODUCTION ?? 0)),
     AUTORIZATION: {
         USER: import.meta.env.AUTH_BASIC_USER,
